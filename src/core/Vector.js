8dc7df4fb4fc5b85e825eac8d060ef9212dbe11d
Shuo Zheng <shuo.s.zheng@gmail.com>
2015-04-17 16:26:32 -0400

    forgot to add Vector.js

---
+++ src/core/Vector.js
@@ -0,0 +1,47 @@
+
+//DEFINITION
+iio.Vector = function(){ this.Vector.apply(this, arguments) };
+
+//CONSTRUCTOR
+iio.Vector.prototype.Vector = function(v,y) {
+	if(v instanceof Array){
+		this.x = v[0] || 0;
+		this.y = v[1] || 0;
+	} else if(v&&v.x) {
+		this.x = v.x || 0;
+		this.y = v.y || 0;
+	} else {
+		this.x = v || 0;
+		this.y = y || 0;
+	}
+}
+
+//STATIC FUNCTIONS
+iio.Vector.add = function(v1, v2) {
+	for (var p in v2)
+	  if (v1[p]) v1[p] += v2[p];
+	return v1
+}
+iio.Vector.sub = function(v1, v2) {
+	for (var p in v2)
+	  if (v1[p]) v1[p] -= v2[p];
+	return v1
+}
+iio.Vector.mult = function(v1, v2) {
+	for (var p in v2)
+	  if (v1[p]) v1[p] *= v2[p];
+	return v1
+}
+iio.Vector.div = function(v1, v2) {
+	for (var p in v2)
+	  if (v1[p]) v1[p] /= v2[p];
+	return v1
+}
+iio.Vector.dist = function(v1, v2) {
+	return Math.sqrt(Math.pow(v2.x - v1.x, 2) + Math.pow(v2.y - v1.y, 2))
+}
+
+//OBJECT FUNCTIONS
+iio.Vector.prototype.clone = function(){
+	return new iio.Vector(this.x,this.y)
+}
