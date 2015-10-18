63b2ae6d7627a02bdbf43ba94c65b05e82099eef
sbiermanlytle <sbiermanlytle@gmail.com>
2015-10-17 19:00:31 -0700

    add ellipseXellipse collisions

--- src/core/libs.js
+++ src/core/libs.js
@@ -310,7 +310,7 @@ iio.collision = {
     } else if (o1 instanceof iio.Ellipse && o2 instanceof iio.Ellipse){
       if ((!o1.vRadius||o1.radius === o1.vRadius) && (!o2.vRadius||o2.radius === o2.vRadius) )
         return iio.collision.circleXcircle(o1,o2)
-      //else http://yehar.com/blog/?p=2926
+      else return iio.collision.ellipseXellipse(o1,o2)
     } else if (o1 instanceof iio.Line && o2 instanceof iio.Line)
       return iio.collision.lineXline(o1,o2)
   },
@@ -392,6 +392,172 @@ iio.collision = {
     } 
     return false;
   },
+  /* Ellipse collision detection
+   * based on script by Olli Niemitalo in 2012-08-06.
+   * This work is placed in the public domain.
+   * http://yehar.com/blog/?p=2926
+   */
+  ellipseXellipse: function(o1,o2){
+    var x0 = o1.pos.x;
+    var y0 = o1.pos.y;
+    var x1 = o2.pos.x;
+    var y1 = o2.pos.y;
+    var w0 = o1.localizeRotation(new iio.Vector(o1.radius, 0),true);
+    var w1 = o2.localizeRotation(new iio.Vector(o2.radius, 0),true);
+    var wx0 = w0.x;
+    var wy0 = w0.y;
+    var wx1 = w1.x;
+    var wy1 = w1.y;
+    var hw0 = o1.vRadius / o1.radius;
+    var hw1 = o2.vRadius / o2.radius;
+
+    var maxIterations = o1.maxCollisionIterations || 10;
+    if (o2.maxCollisionIterations 
+      && o2.maxCollisionIterations > maxIterations)
+      maxIterations = o2.maxCollisionIterations;
+    var numNodes = o1.numCollisionNodes || 10;
+    if (o2.numCollisionNodes 
+      && o2.numCollisionNodes > numNodes)
+      numNodes = o2.numCollisionNodes;
+    var innerPolyCoef = [];
+    var outerPolyCoef = [];
+    for (var t = 0; t <= maxIterations; t++) {
+      innerPolyCoef[t] = 0.5/Math.cos(4*Math.acos(0.0)/numNodes);
+      outerPolyCoef[t] = 0.5/(Math.cos(2*Math.acos(0.0)/numNodes)*Math.cos(2*Math.acos(0.0)/numNodes));
+    }
+    var options = {
+      maxIterations: maxIterations,
+      innerPolyCoef: innerPolyCoef,
+      outerPolyCoef: outerPolyCoef,
+    };
+
+    var rr = hw1*hw1*(wx1*wx1 + wy1*wy1)*(wx1*wx1 + wy1*wy1)*(wx1*wx1 + wy1*wy1);
+    var x = hw1*wx1*(wy1*(y1 - y0) + wx1*(x1 - x0)) - wy1*(wx1*(y1 - y0) - wy1*(x1 - x0));
+    var y = hw1*wy1*(wy1*(y1 - y0) + wx1*(x1 - x0)) + wx1*(wx1*(y1 - y0) - wy1*(x1 - x0));
+    var temp = wx0;
+    wx0 = hw1*wx1*(wy1*wy0 + wx1*wx0) - wy1*(wx1*wy0 - wy1*wx0);
+    var temp2 = wy0;
+    wy0 = hw1*wy1*(wy1*wy0 + wx1*temp) + wx1*(wx1*wy0 - wy1*temp);
+    var hx0 = hw1*wx1*(wy1*(temp*hw0)-wx1*temp2*hw0)-wy1*(wx1*(temp*hw0)+wy1*temp2*hw0);
+    var hy0 = hw1*wy1*(wy1*(temp*hw0)-wx1*temp2*hw0)+wx1*(wx1*(temp*hw0)+wy1*temp2*hw0);
+
+    if (wx0*y - wy0*x < 0) {
+      x = -x;
+      y = -y;
+    }
+                
+    if ((wx0 - x)*(wx0 - x) + (wy0 - y)*(wy0 - y) <= rr) {
+      return true;
+    } else if ((wx0 + x)*(wx0 + x) + (wy0 + y)*(wy0 + y) <= rr) {
+      return true;
+    } else if ((hx0 - x)*(hx0 - x) + (hy0 - y)*(hy0 - y) <= rr) {
+      return true;
+    } else if ((hx0 + x)*(hx0 + x) + (hy0 + y)*(hy0 + y) <= rr) {
+      return true;
+    } else if (x*(hy0 - wy0) + y*(wx0 - hx0) <= hy0*wx0 - hx0*wy0 &&
+               y*(wx0 + hx0) - x*(wy0 + hy0) <= hy0*wx0 - hx0*wy0) {
+      return true;
+    } else if (x*(wx0-hx0) - y*(hy0-wy0) > hx0*(wx0-hx0) - hy0*(hy0-wy0)     
+               && x*(wx0-hx0) - y*(hy0-wy0) < wx0*(wx0-hx0) - wy0*(hy0-wy0)
+               && (x*(hy0-wy0) + y*(wx0-hx0) - hy0*wx0 + hx0*wy0)*(x*(hy0-wy0) + y*(wx0-hx0) - hy0*wx0 + hx0*wy0)
+               <= rr*((wx0-hx0)*(wx0-hx0) + (wy0-hy0)*(wy0-hy0))) {
+      return true;
+    } else if (x*(wx0+hx0) + y*(wy0+hy0) > -wx0*(wx0+hx0) - wy0*(wy0+hy0)
+               && x*(wx0+hx0) + y*(wy0+hy0) < hx0*(wx0+hx0) + hy0*(wy0+hy0)
+               && (y*(wx0+hx0) - x*(wy0+hy0) - hy0*wx0 + hx0*wy0)*(y*(wx0+hx0) - x*(wy0+hy0) - hy0*wx0 + hx0*wy0)
+               <= rr*((wx0+hx0)*(wx0+hx0) + (wy0+hy0)*(wy0+hy0))) {
+      return true;
+    } else {
+      if ((hx0-wx0 - x)*(hx0-wx0 - x) + (hy0-wy0 - y)*(hy0-wy0 - y) <= rr) {
+        return iio.collision.iterate(options,x, y, hx0, hy0, -wx0, -wy0, rr);
+      } else if ((hx0+wx0 - x)*(hx0+wx0 - x) + (hy0+wy0 - y)*(hy0+wy0 - y) <= rr) {
+        return iio.collision.iterate(options,x, y, wx0, wy0, hx0, hy0, rr);
+      } else if ((wx0-hx0 - x)*(wx0-hx0 - x) + (wy0-hy0 - y)*(wy0-hy0 - y) <= rr) {
+        return iio.collision.iterate(options,x, y, -hx0, -hy0, wx0, wy0, rr);
+      } else if ((-wx0-hx0 - x)*(-wx0-hx0 - x) + (-wy0-hy0 - y)*(-wy0-hy0 - y) <= rr) {
+        return iio.collision.iterate(options,x, y, -wx0, -wy0, -hx0, -hy0, rr);
+      } else if (wx0*y - wy0*x < wx0*hy0 - wy0*hx0 && Math.abs(hx0*y - hy0*x) < hy0*wx0 - hx0*wy0) {
+        if (hx0*y - hy0*x > 0) {
+          return iio.collision.iterate(options,x, y, hx0, hy0, -wx0, -wy0, rr);
+        }
+        return iio.collision.iterate(options,x, y, wx0, wy0, hx0, hy0, rr);
+      } else if (wx0*x + wy0*y > wx0*(hx0-wx0) + wy0*(hy0-wy0) && wx0*x + wy0*y < wx0*(hx0+wx0) + wy0*(hy0+wy0)
+                 && (wx0*y - wy0*x - hy0*wx0 + hx0*wy0)*(wx0*y - wy0*x - hy0*wx0 + hx0*wy0) < rr*(wx0*wx0 + wy0*wy0)) {
+        if (wx0*x + wy0*y > wx0*hx0 + wy0*hy0) {
+          return iio.collision.iterate(options,x, y, wx0, wy0, hx0, hy0, rr);
+        }
+        return iio.collision.iterate(options,x, y, hx0, hy0, -wx0, -wy0, rr);
+      } else {
+        if (hx0*y - hy0*x < 0) {
+          x = -x;
+          y = -y;
+        }  
+        if (hx0*x + hy0*y > -hx0*(wx0+hx0) - hy0*(wy0+hy0) && hx0*x + hy0*y < hx0*(hx0-wx0) + hy0*(hy0-wy0)
+            && (hx0*y - hy0*x - hy0*wx0 + hx0*wy0)*(hx0*y - hy0*x - hy0*wx0 + hx0*wy0) < rr*(hx0*hx0 + hy0*hy0)) {
+          if (hx0*x + hy0*y > -hx0*wx0 - hy0*wy0) {      
+            return iio.collision.iterate(options,x, y, hx0, hy0, -wx0, -wy0, rr);
+          } 
+          return iio.collision.iterate(options,x, y, -wx0, -wy0, -hx0, -hy0, rr);
+        }
+        return false;
+      }
+    }
+  },
+  iterate: function(op,x,y,c0x,c0y,c2x,c2y,rr){
+    for (var t=1; t<=op.maxIterations; t++) {
+      var c1x = (c0x + c2x)*op.innerPolyCoef[t];
+      var c1y = (c0y + c2y)*op.innerPolyCoef[t];
+      var tx = x-c1x;
+      var ty = y-c1y;
+      if (tx*tx + ty*ty <= rr) {
+        return true;
+      }
+      var t2x = c2x-c1x;
+      var t2y = c2y-c1y;
+      if (tx*t2x + ty*t2y >= 0 && tx*t2x + ty*t2y <= t2x*t2x + t2y*t2y &&
+          (ty*t2x - tx*t2y >= 0 || rr*(t2x*t2x + t2y*t2y) >= (ty*t2x - tx*t2y)*(ty*t2x - tx*t2y))) {
+        return true;
+      }
+      var t0x = c0x-c1x;
+      var t0y = c0y-c1y;
+      if (tx*t0x + ty*t0y >= 0 && tx*t0x + ty*t0y <= t0x*t0x + t0y*t0y &&
+          (ty*t0x - tx*t0y <= 0 || rr*(t0x*t0x + t0y*t0y) >= (ty*t0x - tx*t0y)*(ty*t0x - tx*t0y))) {
+        return true;
+      }    
+      var c3x = (c0x+c1x)*op.outerPolyCoef[t];
+      var c3y = (c0y+c1y)*op.outerPolyCoef[t];
+      if ((c3x-x)*(c3x-x) + (c3y-y)*(c3y-y) < rr) {
+        c2x = c1x;
+        c2y = c1y;
+        continue;
+      }
+      var c4x = c1x-c3x+c1x;
+      var c4y = c1y-c3y+c1y;
+      if ((c4x-x)*(c4x-x) + (c4y-y)*(c4y-y) < rr) {
+        c0x = c1x;
+        c0y = c1y;
+        continue;
+      }
+      var t3x = c3x-c1x;
+      var t3y = c3y-c1y;
+      if (ty*t3x - tx*t3y <= 0 || rr*(t3x*t3x + t3y*t3y) > (ty*t3x - tx*t3y)*(ty*t3x - tx*t3y)) {
+        if (tx*t3x + ty*t3y > 0) {
+          if (Math.abs(tx*t3x + ty*t3y) <= t3x*t3x + t3y*t3y || (x-c3x)*(c0x-c3x) + (y-c3y)*(c0y-c3y) >= 0) {
+            c2x = c1x;
+            c2y = c1y;
+            continue;
+          }
+        } else if (-(tx*t3x + ty*t3y) <= t3x*t3x + t3y*t3y || (x-c4x)*(c2x-c4x) + (y-c4y)*(c2y-c4y) >= 0) {
+          c0x = c1x;
+          c0y = c1y;
+          continue;
+        }
+      }
+      return false;
+    }
+    // Out of iterations so it is unsure if there was a collision.
+    return false;
+  }
 }
 
 iio.draw = {
